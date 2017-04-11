764a7ebb948759e410d1ca072739b37f4cfbdedd
严俊东 <1172482914@qq.com>
2017-04-11 16:29:46 +0800

    数据库介入

--- client/main.js
+++ client/main.js
@@ -5,6 +5,9 @@ import {Vue} from 'meteor/akryum:vue';
 // Main app
 import App from '/imports/ui/App.vue';
 
+import VueTracker from 'vue-meteor-tracker';
+Vue.use(VueTracker);
+
 Meteor.startup(() => {
   new Vue({
     render: h => h(App),
--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,22 +1,48 @@
 <template>
   <div>
-    test
-
-    <my-button :label="'按钮'" @click="myClick"></my-button>
+    <my-button label="Session测试按钮" @click="myClick"></my-button>
+    <br><br>
+    <input type="text" v-model="msg">
+    <my-button label="Messages测试按钮" @click="addMsg"></my-button>
+    <code>{{msg}}</code>
+    <ul>
+      <li v-for="item in messages">
+        <span v-text="item.message"></span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
   import {Session} from 'meteor/session'
-  console.log(Session)
   export default {
     name: 'App',
+    data(){
+      return {
+        msg: '',
+        messages: []
+      }
+    },
+    meteor: {
+      subscribe: {
+        'messages': [],
+      },
+      messages() {
+        return Messages.find({}, {
+          sort: { date: -1 },
+        });
+      },
+    },
     created(){
       Session.setDefault('gaga', 0)
     },
-    methods:{
+    methods: {
       myClick(){
         console.log(Session.get('gaga'))
+      },
+      addMsg(){
+        Meteor.call('addMessage', this.msg);
+        this.msg = '';
       }
     }
   }
